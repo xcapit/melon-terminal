26805b62d815e73a8d4e4af26e0ea031fdcdd428
Sebastian Siemssen <fubhy@fubhy.com>
2019-11-06 11:13:01 +0100

    Remove console.log.

--- src/graphql/resolvers/PriceSource.ts
+++ src/graphql/resolvers/PriceSource.ts
@@ -2,12 +2,6 @@ import { Resolver } from '~/graphql';
 import { CanonicalPriceFeed } from '@melonproject/melonjs';
 
 export const address: Resolver<CanonicalPriceFeed> = source => source.contract.address;
-export const lastUpdate: Resolver<CanonicalPriceFeed> = async (source, _, context) => {
-  try {
-    const result = await source.getLastUpdate(context.block);
-    console.log(result);
-    return result;
-  } catch (e) {
-    console.log(e);
-  }
+export const lastUpdate: Resolver<CanonicalPriceFeed> = (source, _, context) => {
+  return source.getLastUpdate(context.block);
 };

04976aaa3c2d3803ed46302213ac58aeb2423a61
Sebastian Siemssen <fubhy@fubhy.com>
2020-01-08 12:08:09 +0100

    Fixing asset admin page.

--- src/components/Routes/Fund/FundRoutes/FundInvestmentAssets/FundInvestmentAssets.tsx
+++ src/components/Routes/Fund/FundRoutes/FundInvestmentAssets/FundInvestmentAssets.tsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import * as Yup from 'yup';
+import useForm, { FormContext } from 'react-hook-form';
+import { Participation } from '@melonproject/melonjs';
+import { AllowedInvestmentAsset } from '@melonproject/melongql';
 import { useEnvironment } from '~/hooks/useEnvironment';
 import { useTransaction } from '~/hooks/useTransaction';
 import { ButtonBlock } from '~/components/Common/Form/ButtonBlock/ButtonBlock';
-import { Participation } from '@melonproject/melonjs';
 import { SubmitButton } from '~/components/Common/Form/SubmitButton/SubmitButton';
 import { TransactionModal } from '~/components/Common/TransactionModal/TransactionModal';
 import { useOnChainQueryRefetcher } from '~/hooks/useOnChainQueryRefetcher';
 import { Block } from '~/storybook/components/Block/Block';
 import { Grid, GridRow, GridCol } from '~/storybook/components/Grid/Grid';
 import { SectionTitle } from '~/storybook/components/Title/Title';
-import useForm, { FormContext } from 'react-hook-form';
 import { Spinner } from '~/components/Common/Spinner/Spinner';
 import { useAccount } from '~/hooks/useAccount';
 import { useFundInvestmentAssetsQuery } from '~/queries/FundInvestmentAssets';
-import { AllowedInvestmentAsset } from '@melonproject/melongql';
 
 export interface InvestmentAssetsProps {
   address: string;
@@ -28,10 +28,7 @@ export const InvestmentAssets: React.FC<InvestmentAssetsProps> = ({ address }) =
   const environment = useEnvironment()!;
   const account = useAccount();
   const refetch = useOnChainQueryRefetcher();
-  const [details, _] = useFundInvestmentAssetsQuery(address);
-
-  const allowedAssets = details?.fund?.routes?.participation?.allowedAssets;
-
+  const [details, query] = useFundInvestmentAssetsQuery(address);
   const [addAssets, setAddAssets] = useState<string[]>([]);
   const [removeAssets, setRemoveAssets] = useState<string[]>([]);
 
@@ -50,14 +47,14 @@ export const InvestmentAssets: React.FC<InvestmentAssetsProps> = ({ address }) =
     const participationAddress = details?.fund?.routes?.participation?.address;
     const participation = new Participation(environment, participationAddress);
 
-    if (removeAssets?.length) {
+    if (removeAssets && removeAssets.length) {
       const tx = participation.disableInvestment(account.address!, removeAssets);
       transaction.start(tx, 'Remove assets');
-    } else if (addAssets?.length) {
+    } else if (addAssets && addAssets.length) {
       const tx = participation.enableInvestment(account.address!, addAssets);
       transaction.start(tx, 'Add assets');
     }
-  }, [details, addAssets, removeAssets]);
+  }, [addAssets, removeAssets]);
 
   const validationSchema = Yup.object().shape({
     assets: Yup.array<string>()
@@ -72,29 +69,19 @@ export const InvestmentAssets: React.FC<InvestmentAssetsProps> = ({ address }) =
     reValidateMode: 'onBlur',
   });
 
+  const allowedAssets = details?.fund?.routes?.participation?.allowedAssets || [];
   const defaultValues = useMemo(
-    () =>
-      environment.tokens.map(token => {
-        if (
-          allowedAssets?.some((allowed: AllowedInvestmentAsset) => allowed && allowed.token.symbol === token.symbol)
-        ) {
-          return true;
-        } else {
-          return false;
-        }
-      }),
+    () => environment.tokens.map(token => allowedAssets?.some((allowed: AllowedInvestmentAsset) => allowed && allowed.token!.symbol === token.symbol)),
     [environment, allowedAssets]
   );
 
   const submit = form.handleSubmit(async data => {
+    const assetsToAdd = data.assets.filter(selected => selected && !allowedAssets?.some(available => available.token!.address === selected));
     const assetsToRemove = (allowedAssets || [])
-      .filter(asset => !data.assets.some(selected => selected === asset.token.address))
-      .map(item => item.token.address) as string[];
-    setRemoveAssets(assetsToRemove);
+      .filter(asset => !data.assets.some(selected => selected === asset.token!.address))
+      .map(item => item.token!.address) as string[];
 
-    const assetsToAdd = data.assets.filter(
-      selected => selected && !allowedAssets?.some(available => available.token.address === selected)
-    );
+    setRemoveAssets(assetsToRemove);
     setAddAssets(assetsToAdd);
   });
 
@@ -108,36 +95,36 @@ export const InvestmentAssets: React.FC<InvestmentAssetsProps> = ({ address }) =
                 <SectionTitle>Define investment assets</SectionTitle>
 
                 {form.errors.assets && <p>{form.errors.assets.message}</p>}
-                {allowedAssets ? (
-                  <>
-                    <ul>
-                      {environment.tokens.map((token, index) => (
-                        <li key={token.symbol}>
-                          <input
-                            defaultChecked={defaultValues[index]}
-                            id={`assets[${index}]`}
-                            type="checkbox"
-                            name={`assets[${index}]`}
-                            value={token.address}
-                            key={token.address}
-                            ref={form.register}
-                          />
-                          <label htmlFor={`assets[${index}]`}>
-                            {token.symbol} ({token.name})
-                          </label>
-                        </li>
-                      ))}
-                    </ul>
-                    <ButtonBlock>
-                      <SubmitButton type="button" label="Set investment assets" onClick={submit} />
-                    </ButtonBlock>
-                  </>
-                ) : (
-                  <Spinner></Spinner>
+                {query.loading && <Spinner />}
+
+                {!query.loading && (
+                  <ul>
+                    {environment.tokens.map((token, index) => (
+                      <li key={token.symbol}>
+                        <input
+                          defaultChecked={defaultValues[index]}
+                          id={`assets[${index}]`}
+                          type="checkbox"
+                          name={`assets[${index}]`}
+                          value={token.address}
+                          key={token.address}
+                          ref={form.register}
+                        />
+                        <label htmlFor={`assets[${index}]`}>
+                          {token.symbol} ({token.name})
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
                 )}
+
+                <ButtonBlock>
+                  <SubmitButton type="button" label="Set investment assets" onClick={submit} />
+                </ButtonBlock>
               </form>
             </FormContext>
           </Block>
+
           <TransactionModal transaction={transaction} />
         </GridCol>
       </GridRow>

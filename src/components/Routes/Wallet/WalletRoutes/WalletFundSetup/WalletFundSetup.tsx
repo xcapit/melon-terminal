ec3dbdb191f5230a3c49e6663de3c04dfdad3487
ishak-lm <53437205+ishak-lm@users.noreply.github.com>
2020-01-08 11:43:42 +0100

    Grid and block component in create fund page (#114)

--- src/components/Routes/Wallet/WalletRoutes/WalletFundSetup/WalletFundSetup.tsx
+++ src/components/Routes/Wallet/WalletRoutes/WalletFundSetup/WalletFundSetup.tsx
@@ -12,6 +12,9 @@ import { useOnChainQueryRefetcher } from '~/hooks/useOnChainQueryRefetcher';
 import { Input } from '~/storybook/components/Input/Input';
 import { FormField } from '~/storybook/components/FormField/FormField';
 import { Button } from '~/storybook/components/Button/Button';
+import { Grid, GridRow, GridCol } from '~/storybook/components/Grid/Grid';
+import { SectionTitle } from '~/storybook/components/Title/Title';
+import { Block } from '~/storybook/components/Block/Block';
 
 export interface WalletFundSetupForm {
   name: string;
@@ -97,94 +100,117 @@ export const WalletFundSetup: React.FC = () => {
   });
 
   return (
-    <>
-      <h1>Fund</h1>
+    <Grid>
       <FormContext {...form}>
         <form onSubmit={submit}>
-          <FormField name="name" label="Name">
-            <Input id="name" name="name" type="text" />
-          </FormField>
+          <GridRow>
+            <GridCol xs={12} sm={6}>
+              <Block>
+                <SectionTitle>Fund</SectionTitle>
+                <FormField name="name" label="Name">
+                  <Input id="name" name="name" type="text" />
+                </FormField>
+              </Block>
+            </GridCol>
+            <GridCol xs={12} sm={6}>
+              <Block>
+                <SectionTitle>Fees</SectionTitle>
+                <FormField name="managementFee" label="Management Fee (%)">
+                  <Input id="managementFee" name="managementFee" type="number" />
+                </FormField>
 
-          <h3>Fees</h3>
+                <FormField name="performanceFee" label="Performance Fee (%)">
+                  <Input id="performanceFee" name="performanceFee" type="number" />
+                </FormField>
 
-          <FormField name="managementFee" label="Management Fee (%)">
-            <Input id="managementFee" name="managementFee" type="number" />
-          </FormField>
+                <FormField name="performanceFeePeriod" label="Performance Fee Period (days)">
+                  <Input id="performanceFeePeriod" name="performanceFeePeriod" type="number" />
+                </FormField>
+              </Block>
+            </GridCol>
+          </GridRow>
 
-          <FormField name="performanceFee" label="Performance Fee (%)">
-            <Input id="performanceFee" name="performanceFee" type="number" />
-          </FormField>
+          <GridRow>
+            <GridCol xs={12} sm={6}>
+              <Block>
+                <SectionTitle>Supported exchanges</SectionTitle>
+                {form.errors.exchanges && <p>{form.errors.exchanges.message}</p>}
+                <ul>
+                  {environment.exchanges.map((exchange, index) => (
+                    <li key={exchange.id}>
+                      <input
+                        id={`exchanges[${index}]`}
+                        type="checkbox"
+                        name={`exchanges[${index}]`}
+                        value={exchange.id}
+                        key={exchange.id}
+                        ref={form.register}
+                      />
+                      <label htmlFor={`exchanges[${index}]`}>{exchange.name}</label>
+                    </li>
+                  ))}
+                </ul>
+              </Block>
+            </GridCol>
+            <GridCol xs={12} sm={6}>
+              <Block>
+                <SectionTitle>Allowed investment assets</SectionTitle>
+                {form.errors.assets && <p>{form.errors.assets.message}</p>}
+                <ul>
+                  {environment.tokens.map((token, index) => (
+                    <li key={token.symbol}>
+                      <input
+                        id={`assets[${index}]`}
+                        type="checkbox"
+                        name={`assets[${index}]`}
+                        value={token.symbol}
+                        key={token.symbol}
+                        ref={form.register}
+                      />
+                      <label htmlFor={`assets[${index}]`}>
+                        {token.symbol} ({token.name})
+                      </label>
+                    </li>
+                  ))}
+                </ul>
+              </Block>
+            </GridCol>
+          </GridRow>
 
-          <FormField name="performanceFeePeriod" label="Performance Fee Period (days)">
-            <Input id="performanceFeePeriod" name="performanceFeePeriod" type="number" />
-          </FormField>
+          <GridRow>
+            <GridCol xs={12} sm={12}>
+              <Block>
+                <SectionTitle>Disclaimer</SectionTitle>
+                <p>
+                  IMPORTANT NOTE: PLEASE READ THE FULL VERSION OF THIS DISCLAIMER CAREFULLY BEFORE USING THE MELON
+                  PROTOCOL.
+                </p>
+                <p>
+                  YOUR USE OF THE MELON PROTOCOL AND/OR THE SOFTWARE MAY BE SUBJECT TO THE FINANCIAL LAWS AND
+                  REGULATIONS OF VARIOUS JURISDICTIONS. PRIOR TO USING THE MELON PROTOCOL, SEEK LEGAL ASSISTANCE TO
+                  ASSURE THAT YOU MAY USE THE SOFTWARE IN COMPLIANCE WITH APPLICABLE LAW. FAILURE TO DO SO MAY SUBJECT
+                  YOU TO CRIMINAL AS WELL AS CIVIL PENALTIES IN ONE OR MORE JURISDICTIONS. BY USING THIS SOFTWARE, YOU
+                  CONFIRM THAT YOU HAVE SOUGHT THE ADVICE OF COMPETENT COUNSEL OR ARE OTHERWISE FAMILIAR WITH THE
+                  APPLICABLE LAWS AND REGULATIONS PERTAINING TO YOUR INTENDED USE OF THE MELON PROTOCOL. BY USING THIS
+                  SOFTWARE, YOU UNDERSTAND, ACKNOWLEDGE AND ACCEPT THAT THE MELON PROTOCOL AND/OR THE UNDERLYING
+                  SOFTWARE ARE PROVIDED “AS IS” AND WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND EITHER EXPRESSED
+                  OR IMPLIED. ANY USE OF THIS OPEN SOURCE SOFTWARE RELEASED UNDER THE GNU GENERAL PUBLIC LICENSE VERSION
+                  3 (GPL 3) IS DONE AT YOUR OWN RISK TO THE FULLEST EXTENT PERMISSIBLE PURSUANT TO APPLICABLE LAW ANY
+                  AND ALL LIABILITY AS WELL AS ALL WARRANTIES, INCLUDING ANY FITNESS FOR A PARTICULAR PURPOSE WITH
+                  RESPECT TO THE MELON PROTOCOL AND/OR THE UNDERLYING SOFTWARE AND THE USE THEREOF ARE DISCLAIMED.
+                </p>
 
-          <h3>Supported exchanges</h3>
-          {form.errors.exchanges && <p>{form.errors.exchanges.message}</p>}
-          <ul>
-            {environment.exchanges.map((exchange, index) => (
-              <li key={exchange.id}>
-                <input
-                  id={`exchanges[${index}]`}
-                  type="checkbox"
-                  name={`exchanges[${index}]`}
-                  value={exchange.id}
-                  key={exchange.id}
-                  ref={form.register}
-                />
-                <label htmlFor={`exchanges[${index}]`}>{exchange.name}</label>
-              </li>
-            ))}
-          </ul>
+                <input id="termsAndConditions" type="checkbox" name="termsAndConditions" ref={form.register} />
+                <label htmlFor="termsAndConditions">I accept the terms and conditions</label>
 
-          <h3>Allowed investment assets</h3>
-          {form.errors.assets && <p>{form.errors.assets.message}</p>}
-          <ul>
-            {environment.tokens.map((token, index) => (
-              <li key={token.symbol}>
-                <input
-                  id={`assets[${index}]`}
-                  type="checkbox"
-                  name={`assets[${index}]`}
-                  value={token.symbol}
-                  key={token.symbol}
-                  ref={form.register}
-                />
-                <label htmlFor={`assets[${index}]`}>
-                  {token.symbol} ({token.name})
-                </label>
-              </li>
-            ))}
-          </ul>
-
-          <h3>Disclaimer</h3>
-          <p>
-            IMPORTANT NOTE: PLEASE READ THE FULL VERSION OF THIS DISCLAIMER CAREFULLY BEFORE USING THE MELON PROTOCOL.
-          </p>
-          <p>
-            YOUR USE OF THE MELON PROTOCOL AND/OR THE SOFTWARE MAY BE SUBJECT TO THE FINANCIAL LAWS AND REGULATIONS OF
-            VARIOUS JURISDICTIONS. PRIOR TO USING THE MELON PROTOCOL, SEEK LEGAL ASSISTANCE TO ASSURE THAT YOU MAY USE
-            THE SOFTWARE IN COMPLIANCE WITH APPLICABLE LAW. FAILURE TO DO SO MAY SUBJECT YOU TO CRIMINAL AS WELL AS
-            CIVIL PENALTIES IN ONE OR MORE JURISDICTIONS. BY USING THIS SOFTWARE, YOU CONFIRM THAT YOU HAVE SOUGHT THE
-            ADVICE OF COMPETENT COUNSEL OR ARE OTHERWISE FAMILIAR WITH THE APPLICABLE LAWS AND REGULATIONS PERTAINING TO
-            YOUR INTENDED USE OF THE MELON PROTOCOL. BY USING THIS SOFTWARE, YOU UNDERSTAND, ACKNOWLEDGE AND ACCEPT THAT
-            THE MELON PROTOCOL AND/OR THE UNDERLYING SOFTWARE ARE PROVIDED “AS IS” AND WITHOUT WARRANTIES OR
-            REPRESENTATIONS OF ANY KIND EITHER EXPRESSED OR IMPLIED. ANY USE OF THIS OPEN SOURCE SOFTWARE RELEASED UNDER
-            THE GNU GENERAL PUBLIC LICENSE VERSION 3 (GPL 3) IS DONE AT YOUR OWN RISK TO THE FULLEST EXTENT PERMISSIBLE
-            PURSUANT TO APPLICABLE LAW ANY AND ALL LIABILITY AS WELL AS ALL WARRANTIES, INCLUDING ANY FITNESS FOR A
-            PARTICULAR PURPOSE WITH RESPECT TO THE MELON PROTOCOL AND/OR THE UNDERLYING SOFTWARE AND THE USE THEREOF ARE
-            DISCLAIMED.
-          </p>
-
-          <input id="termsAndConditions" type="checkbox" name="termsAndConditions" ref={form.register} />
-          <label htmlFor="termsAndConditions">I accept the terms and conditions</label>
-
-          <Button type="submit">Create fund</Button>
+                <Button type="submit">Create fund</Button>
+              </Block>
+            </GridCol>
+          </GridRow>
         </form>
       </FormContext>
-
       <TransactionModal transaction={transaction} />
-    </>
+    </Grid>
   );
 };
 

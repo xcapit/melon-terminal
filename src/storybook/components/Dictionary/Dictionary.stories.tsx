620eaaa8f4b5cd4bc6bc5e71b1643ef87b028dff
Beatriz Beato <beatrizbeatosilva@gmail.com>
2020-01-09 05:16:08 +0000

    Dictionary (#132)

---
+++ src/storybook/components/Dictionary/Dictionary.stories.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import { Grid, GridRow, GridCol } from '../Grid/Grid';
+import { SectionTitle } from '../Title/Title';
+import {
+  Dictionary,
+  DictionaryDivider,
+  DictionaryEntry,
+  DictionaryLabel,
+  DictionaryData,
+} from './Dictionary';
+
+
+export default { title: 'Components|Dictionary' };
+
+
+export const Default: React.FC = () => {
+  return (
+    <Dictionary>
+      <DictionaryEntry>
+        <DictionaryLabel>Label</DictionaryLabel>
+        <DictionaryData>Data</DictionaryData>
+      </DictionaryEntry>
+      <DictionaryEntry>
+        <DictionaryLabel>Label</DictionaryLabel>
+        <DictionaryData>Data</DictionaryData>
+      </DictionaryEntry>
+      <DictionaryEntry>
+        <DictionaryLabel>Label</DictionaryLabel>
+        <DictionaryData>Data</DictionaryData>
+      </DictionaryEntry>
+      <DictionaryDivider />
+      <DictionaryEntry>
+        <DictionaryLabel>Label</DictionaryLabel>
+        <DictionaryData>Data</DictionaryData>
+      </DictionaryEntry>
+      <DictionaryEntry>
+        <DictionaryLabel>Label</DictionaryLabel>
+        <DictionaryData>Data</DictionaryData>
+      </DictionaryEntry>
+    </Dictionary>
+  );
+};
+
+export const WithContextAndData: React.FC = () => {
+  return (
+    <Grid>
+      <GridRow>
+        <GridCol xs={12} md={6}>
+          <Dictionary>
+            <SectionTitle>Fund Factsheet</SectionTitle>
+            <DictionaryDivider />
+            <DictionaryEntry>
+              <DictionaryLabel>Fund Name</DictionaryLabel>
+              <DictionaryData>My Second Melon Fund</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>Protocol version</DictionaryLabel>
+              <DictionaryData>1.0.5</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>Fund Adress</DictionaryLabel>
+              <DictionaryData>0xcf74a2f1778509fee75444da809</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>Manager Adress</DictionaryLabel>
+              <DictionaryData>0x56a2f1778509fee7547ca809</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>Status</DictionaryLabel>
+              <DictionaryData>Active</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryDivider />
+            <DictionaryEntry>
+              <DictionaryLabel>GAV</DictionaryLabel>
+              <DictionaryData>0.0000 ETH</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>NAV</DictionaryLabel>
+              <DictionaryData>0.0000 ETH</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel># shares</DictionaryLabel>
+              <DictionaryData>0.0000 shares</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>Share Price</DictionaryLabel>
+              <DictionaryData>1.87775</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryDivider />
+            <DictionaryEntry>
+              <DictionaryLabel>Management fee</DictionaryLabel>
+              <DictionaryData>2.00%</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>Performence fee</DictionaryLabel>
+              <DictionaryData>10.00%</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>Performence fee period</DictionaryLabel>
+              <DictionaryData>1 day</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>Start of next Performence</DictionaryLabel>
+              <DictionaryData>01/08/2020 12:24</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryDivider />
+            <DictionaryEntry>
+              <DictionaryLabel>Return since inception</DictionaryLabel>
+              <DictionaryData>0.0000 ETH</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>Annualized return</DictionaryLabel>
+              <DictionaryData>0.0000 ETH</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryEntry>
+              <DictionaryLabel>Annualized volatility</DictionaryLabel>
+              <DictionaryData>62.54%</DictionaryData>
+            </DictionaryEntry>
+            <DictionaryDivider />
+            <DictionaryEntry>
+              <DictionaryLabel>Authorized exchanges</DictionaryLabel>
+              <DictionaryData>Oasisdex, 0x (v2.0), Melon Engine</DictionaryData>
+            </DictionaryEntry>
+          </Dictionary>
+        </GridCol>
+      </GridRow>
+    </Grid>
+  );
+};
---
+++ src/storybook/components/Dictionary/Dictionary.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+import { Block } from '../Block/Block';
+
+export interface DictionaryProps {
+  entries?: Array<{
+    label: React.ReactNode;
+    data: React.ReactNode;
+  }>;
+}
+
+export const Dictionary = styled(Block)<DictionaryProps>``;
+
+export const DictionaryDivider = styled.div`
+  width:100%;
+  height: ${props => props.theme.spaceUnits.m};
+  @media (${props => props.theme.mediaQueries.s}) {
+    height: ${props => props.theme.spaceUnits.l};
+  }
+`;
+
+export const DictionaryEntry = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  margin-bottom: ${props => props.theme.spaceUnits.xs};
+  @media (${props => props.theme.mediaQueries.s}) {
+    flex-wrap: nowrap;
+    margin-bottom: ${props => props.theme.spaceUnits.xxs};
+  }
+`;
+
+export const DictionaryLabel = styled.div`
+  align-self: flex-end;
+  font-size: ${props => props.theme.fontSizes.m};
+  color: ${props => props.theme.mainColors.secondaryDark};
+  @media (${props => props.theme.mediaQueries.s}) {
+    width: 40%;
+  }
+`;
+
+export const DictionaryData = styled.div`
+  align-self: flex-end;
+  font-size: ${props => props.theme.fontSizes.l};
+  width: 100%;
+  @media (${props => props.theme.mediaQueries.s}) {
+    width: 60%;
+    padding-left: ${props => props.theme.spaceUnits.s};
+  }
+`;

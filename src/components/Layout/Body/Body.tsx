2aed63909d3299eb5924e4e923fb4f9f94b2f793
ishak-lm <53437205+ishak-lm@users.noreply.github.com>
2020-01-13 12:32:53 +0100

    Merge body / footer / header component. (#157)

--- src/components/Layout/Body/Body.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import { SkeletonBody } from '~/storybook/components/Skeleton/Skeleton';
-
-export const Body: React.FC = ({ children }) => <SkeletonBody>{children}</SkeletonBody>;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,98 @@
 import React from 'react';
-import { Body } from './Body/Body';
-import { Header } from './Header/Header';
-import { Footer } from './Footer/Footer';
-import { Skeleton } from '~/storybook/components/Skeleton/Skeleton';
+import { useLocation } from 'react-router';
+import { format } from 'date-fns';
+import { usePriceFeedUpdateQuery } from '~/queries/PriceFeedUpdate';
+import { Link } from '~/components/Common/Link/Link';
+import { useAccount } from '~/hooks/useAccount';
+import { useEnvironment } from '~/hooks/useEnvironment';
+import { Skeleton, SkeletonHead, SkeletonBody, SkeletonFeet } from '~/storybook/components/Skeleton/Skeleton';
+import {
+  Header as HeaderContainer,
+  HeaderContent,
+  LogoContainer,
+  Account,
+  AccountAddress,
+  AccountBalance,
+  AccountInfo,
+  AccountNetwork,
+  AccountName,
+} from '~/storybook/components/Header/Header';
+import { Footer, FooterNavigation, FooterItem } from '~/storybook/components/Footer/Footer';
+import { Logo } from '~/storybook/components/Logo/Logo';
 
-export const Layout: React.FC = ({ children }) => (
-  <Skeleton>
-    <Header />
-    <Body>{children}</Body>
-    <Footer />
-  </Skeleton>
-);
+export const Layout: React.FC = ({ children }) => {
+  const [update] = usePriceFeedUpdateQuery();
+  const environment = useEnvironment();
+  const location = useLocation();
+  const account = useAccount();
+  const network = environment ? environment.network : 'OFFLINE';
+
+  return (
+    <Skeleton>
+      <SkeletonHead>
+        <HeaderContainer>
+          <HeaderContent>
+            <LogoContainer>
+              <Link to="/">
+                <Logo name="with-bottom-text" size="small" />
+              </Link>
+            </LogoContainer>
+            <Account>
+              {/* TODO: Remove this component */}
+              <AccountName />
+              <AccountInfo>
+                {account.fund && (
+                  <AccountAddress>
+                    <Link to={`/fund/${account.fund}`} title={account.fund}>
+                      Your fund
+                    </Link>
+                  </AccountAddress>
+                )}
+                {account.address && (
+                  <AccountAddress>
+                    <Link to="/wallet" title={account.address}>
+                      Your wallet
+                    </Link>
+                  </AccountAddress>
+                )}
+                <AccountNetwork>
+                  <Link to={{ pathname: '/connect', state: { redirect: location } }} title="Change connection method">
+                    {network}
+                  </Link>
+                </AccountNetwork>
+                {account.eth && <AccountBalance>{account.eth.toFixed(4)} ETH</AccountBalance>}
+              </AccountInfo>
+            </Account>
+          </HeaderContent>
+        </HeaderContainer>
+      </SkeletonHead>
+      <SkeletonBody>{children}</SkeletonBody>
+      <SkeletonFeet>
+        <Footer>
+          <FooterNavigation>
+            <FooterItem>
+              <a href="https://melonprotocol.com">About</a>
+            </FooterItem>
+            <FooterItem>
+              <a href="https://docs.melonport.com">Documentation</a>
+            </FooterItem>
+            <FooterItem>
+              <a href="https://github.com/Avantgarde-Finance/manager-interface/issues">Report an issue</a>
+            </FooterItem>
+            <FooterItem>
+              <Link to="/playground/onchain">Network explorer</Link>
+            </FooterItem>
+            <FooterItem>
+              <Link to="/playground/thegraph">Graph explorer</Link>
+            </FooterItem>
+            {update && (
+              <FooterItem>
+                <span>Last pricefeed update at {format(update, 'yyyy-MM-dd hh:mm a')}</span>
+              </FooterItem>
+            )}
+          </FooterNavigation>
+        </Footer>
+      </SkeletonFeet>
+    </Skeleton>
+  );
+};

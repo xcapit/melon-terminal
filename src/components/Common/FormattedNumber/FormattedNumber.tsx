bb27ed0a29dde75d0defd42bd7a6f6b486bc872e
Sebastian Siemssen <fubhy@fubhy.com>
2020-01-16 16:26:45 +0100

    Use FormattedNumber in FactSheet.

--- src/components/Common/FormattedNumber/FormattedNumber.tsx
+++ src/components/Common/FormattedNumber/FormattedNumber.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import BigNumber from 'bignumber.js';
 
 export interface FormattedNumberData {
-  value?: BigNumber;
+  value?: BigNumber | number | string | null;
   prefix?: string;
   suffix?: string;
   decimals?: number;
+  colorize?: boolean;
 }
 
-export const FormattedNumber: React.FC<FormattedNumberData> = ({ value, prefix, suffix, decimals = 4 }) => {
+interface ColorProps {
+  color: 'red' | 'green' | 'grey';
+}
+
+const Color = styled.span<ColorProps>`
+  color: ${props => props.theme.otherColors[props.color]};
+`;
+
+export const FormattedNumber: React.FC<FormattedNumberData> = ({ value, prefix, suffix, decimals = 4, colorize = false }) => {
   const bn = BigNumber.isBigNumber(value) ? value : new BigNumber(value ?? 'NaN');
-  if (bn.isNaN()) {
-    return <>N/A</>;
+  const output = bn.isNaN() ? 'N/A' : [prefix, ' ', bn.toFixed(decimals), suffix === '%' ? '' : ' ', suffix];
+
+  if (colorize) {
+    const color = bn.isNaN() || bn.isZero() ? 'grey' : bn.isPositive() ? 'green' : 'red';
+    return <Color color={color}>{output}</Color>
   }
 
-  return <>{[prefix, bn.toFixed(decimals), suffix].join(' ')}</>;
+  return <>{output}</>;
 };
--- src/storybook/components/NumberChange/NumberChange.styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export interface ColorProps {
-  color: 'red' | 'green' | 'grey';
-}
-
-export const Color = styled.span<ColorProps>`
-  color: ${props => props.theme.otherColors[props.color]};
-`;

98b37750f31f911536b2acf87533b4382a6e7cb3
ishak-lm <53437205+ishak-lm@users.noreply.github.com>
2020-01-17 08:52:27 +0100

    Redirect to front page on 404. (#211)

--- src/components/Routes/NoMatch/NoMatch.tsx
+++ src/components/Routes/NoMatch/NoMatch.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { NotificationBar, NotificationContent } from '~/storybook/components/NotificationBar/NotificationBar';
-import { Grid, GridRow, GridCol } from '~/storybook/components/Grid/Grid';
+import { Redirect } from 'react-router';
 
-export const NoMatch: React.FC = () => (
-  <Grid>
-    <GridRow>
-      <GridCol sm={12} xs={12}>
-        <NotificationBar kind="error">
-          <NotificationContent>
-            <span>Oops! We did not find the page you are looking for.</span>
-          </NotificationContent>
-        </NotificationBar>
-      </GridCol>
-    </GridRow>
-  </Grid>
-);
+export const NoMatch: React.FC = () => {
+  return <Redirect to="/" />;
+};
 
 export default NoMatch;

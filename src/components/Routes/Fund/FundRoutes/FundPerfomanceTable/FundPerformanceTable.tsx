45a0da5850a1b969b4b98d3716b0a23a33e10e68
Erin Koen <46381469+erin-koen@users.noreply.github.com>
2020-02-11 17:48:01 -0500

    Performance table copy (#330)

--- src/components/Routes/Fund/FundRoutes/FundPerfomanceTable/FundPerformanceTable.tsx
+++ src/components/Routes/Fund/FundRoutes/FundPerfomanceTable/FundPerformanceTable.tsx
@@ -14,10 +14,17 @@ import { Block } from '~/storybook/components/Block/Block';
 import { SectionTitle } from '~/storybook/components/Title/Title';
 import { useFundPerformanceQuery } from './FundPerformance.query';
 import { FormattedNumber } from '~/components/Common/FormattedNumber/FormattedNumber';
+import { LabelSideInfo } from '~/storybook/components/Label/Label';
+import styled from 'styled-components';
 
 export interface FundPerformanceTableProps {
   address: string;
 }
+
+const TableDescription = styled(LabelSideInfo)`
+  font-size: ${props => props.theme.fontSizes.s};
+  padding: ${props => props.theme.spaceUnits.s};
+`;
 
 export const FundPerformanceTable: React.FC<FundPerformanceTableProps> = ({ address }) => {
   const [fund, assets, query] = useFundPerformanceQuery(address, ['WBTC', 'WETH', 'DAI']);
@@ -37,81 +44,84 @@ export const FundPerformanceTable: React.FC<FundPerformanceTableProps> = ({ addr
 
   return (
     <Block>
-      <SectionTitle>Performance History</SectionTitle>
+      <SectionTitle>Relative Fund Performance</SectionTitle>
 
       <ScrollableTable>
         <Table>
-        <thead>
-          <HeaderRow>
-            <HeaderCell>Relative Performance</HeaderCell>
-            <HeaderCellRightAlign>QTD</HeaderCellRightAlign>
-            <HeaderCellRightAlign>YTD</HeaderCellRightAlign>
-            <HeaderCellRightAlign>1-Month</HeaderCellRightAlign>
-            <HeaderCellRightAlign>6-Month</HeaderCellRightAlign>
-            <HeaderCellRightAlign>1-Year</HeaderCellRightAlign>
-            <HeaderCellRightAlign>Since Fund Inception</HeaderCellRightAlign>
-          </HeaderRow>
-        </thead>
+          <thead>
+            <HeaderRow>
+              <HeaderCell>{null}</HeaderCell>
+              <HeaderCellRightAlign>QTD</HeaderCellRightAlign>
+              <HeaderCellRightAlign>YTD</HeaderCellRightAlign>
+              <HeaderCellRightAlign>1-Month</HeaderCellRightAlign>
+              <HeaderCellRightAlign>6-Month</HeaderCellRightAlign>
+              <HeaderCellRightAlign>1-Year</HeaderCellRightAlign>
+              <HeaderCellRightAlign>Since Fund Inception</HeaderCellRightAlign>
+            </HeaderRow>
+          </thead>
 
-        <tbody>
-          {(assets ?? []).map(asset => (
-            <Fragment key={asset.symbol}>
-              <BodyRow>
-                <BodyCell>
-                  {fund.name} vs. {asset.symbol}
-                </BodyCell>
-                <BodyCellRightAlign>
-                  <FormattedNumber
-                    value={fund.qtdReturn.minus(asset.qtdReturn)}
-                    colorize={true}
-                    decimals={2}
-                    suffix="%"
-                  />
-                </BodyCellRightAlign>
-                <BodyCellRightAlign>
-                  <FormattedNumber
-                    value={fund.ytdReturn.minus(asset.ytdReturn)}
-                    colorize={true}
-                    decimals={2}
-                    suffix="%"
-                  />
-                </BodyCellRightAlign>
-                <BodyCellRightAlign>
-                  <FormattedNumber
-                    value={fund.oneMonthReturn.minus(asset.oneMonthReturn)}
-                    colorize={true}
-                    decimals={2}
-                    suffix="%"
-                  />
-                </BodyCellRightAlign>
-                <BodyCellRightAlign>
-                  <FormattedNumber
-                    value={fund.sixMonthReturn.minus(asset.sixMonthReturn)}
-                    colorize={true}
-                    decimals={2}
-                    suffix="%"
-                  />
-                </BodyCellRightAlign>
-                <BodyCellRightAlign>
-                  <FormattedNumber
-                    value={fund.oneYearReturn.minus(asset.oneYearReturn)}
-                    colorize={true}
-                    decimals={2}
-                    suffix="%"
-                  />
-                </BodyCellRightAlign>
-                <BodyCellRightAlign>
-                  <FormattedNumber
-                    value={fund.returnSinceInception.minus(asset.returnSinceInception)}
-                    colorize={true}
-                    decimals={2}
-                    suffix="%"
-                  />
-                </BodyCellRightAlign>
-              </BodyRow>
-            </Fragment>
-          ))}
-        </tbody>
+          <tbody>
+            {(assets ?? []).map(asset => (
+              <Fragment key={asset.symbol}>
+                <BodyRow>
+                  <BodyCell>
+                    {fund.name} vs. {asset.symbol}
+                  </BodyCell>
+                  <BodyCellRightAlign>
+                    <FormattedNumber
+                      value={fund.qtdReturn.minus(asset.qtdReturn)}
+                      colorize={true}
+                      decimals={2}
+                      suffix="%"
+                    />
+                  </BodyCellRightAlign>
+                  <BodyCellRightAlign>
+                    <FormattedNumber
+                      value={fund.ytdReturn.minus(asset.ytdReturn)}
+                      colorize={true}
+                      decimals={2}
+                      suffix="%"
+                    />
+                  </BodyCellRightAlign>
+                  <BodyCellRightAlign>
+                    <FormattedNumber
+                      value={fund.oneMonthReturn.minus(asset.oneMonthReturn)}
+                      colorize={true}
+                      decimals={2}
+                      suffix="%"
+                    />
+                  </BodyCellRightAlign>
+                  <BodyCellRightAlign>
+                    <FormattedNumber
+                      value={fund.sixMonthReturn.minus(asset.sixMonthReturn)}
+                      colorize={true}
+                      decimals={2}
+                      suffix="%"
+                    />
+                  </BodyCellRightAlign>
+                  <BodyCellRightAlign>
+                    <FormattedNumber
+                      value={fund.oneYearReturn.minus(asset.oneYearReturn)}
+                      colorize={true}
+                      decimals={2}
+                      suffix="%"
+                    />
+                  </BodyCellRightAlign>
+                  <BodyCellRightAlign>
+                    <FormattedNumber
+                      value={fund.returnSinceInception.minus(asset.returnSinceInception)}
+                      colorize={true}
+                      decimals={2}
+                      suffix="%"
+                    />
+                  </BodyCellRightAlign>
+                </BodyRow>
+              </Fragment>
+            ))}
+          </tbody>
+          <tfoot>
+            <TableDescription>Fund share price and assets benchmarked against ETH</TableDescription>
+          </tfoot>
         </Table>
       </ScrollableTable>
     </Block>

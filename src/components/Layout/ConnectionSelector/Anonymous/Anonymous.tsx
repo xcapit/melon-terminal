3e56c0ca928d8051b58d99bffe73353ef5d657e0
Sebastian Siemssen <fubhy@fubhy.com>
2020-02-25 12:30:03 +0100

    Remove recursive network check.

--- src/components/Layout/ConnectionSelector/Anonymous/Anonymous.tsx
+++ src/components/Layout/ConnectionSelector/Anonymous/Anonymous.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import * as Rx from 'rxjs';
-import { equals } from 'ramda';
 import { Eth } from 'web3-eth';
 import { HttpProvider } from 'web3-providers';
 import {
   ConnectionMethod,
   ConnectionAction,
-  networkChanged,
   connectionEstablished,
   ConnectionMethodProps,
 } from '~/components/Contexts/Connection/Connection';
 import {
   map,
-  expand,
-  concatMap,
   retryWhen,
   delay,
   take,
-  distinctUntilChanged,
   share,
-  skip,
-  catchError,
 } from 'rxjs/operators';
 import { networkFromId } from '~/utils/networkFromId';
 import { SectionTitle } from '~/storybook/components/Title/Title';
@@ -43,26 +36,13 @@ const connect = (): Rx.Observable<ConnectionAction> => {
   return Rx.using(create, resource => {
     const eth = (resource as EthResource).eth;
     const connect$ = Rx.defer(async () => networkFromId(await eth.net.getId())).pipe(
-      retryWhen(error => error.pipe(delay(1000))),
+      retryWhen(error => error.pipe(delay(10000))),
       take(1),
       share()
     );
 
-    const enable$ = connect$.pipe(map(network => connectionEstablished(eth, network)));
-    const network$ = connect$.pipe(
-      expand(() =>
-        Rx.timer(1000).pipe(
-          concatMap(() => eth.net.getId()),
-          map(id => networkFromId(id)),
-          catchError(() => Rx.of(undefined))
-        )
-      ),
-      distinctUntilChanged((a, b) => equals(a, b)),
-      map(network => networkChanged(network)),
-      skip(1)
-    );
-
-    return Rx.concat(enable$, network$);
+    const initial$ = connect$.pipe(map(network => connectionEstablished(eth, network)));
+    return initial$;
   });
 };
 
@@ -75,8 +55,8 @@ export const Anonymous: React.FC<ConnectionMethodProps> = ({ connect, active }) 
           Connect
         </Button>
       ) : (
-        <span>Currently selected</span>
-      )}
+          <span>Currently selected</span>
+        )}
     </>
   );
 };

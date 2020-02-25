dd6f8e3d39ba162d040210c1b08493317979acf2
ishak-lm <53437205+ishak-lm@users.noreply.github.com>
2020-02-25 09:30:00 +0100

    Fix color for tooltip (#421)

--- src/storybook/components/Tooltip/Tooltip.styles.tsx
+++ src/storybook/components/Tooltip/Tooltip.styles.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: ${props => props.theme.otherColors.white};
+  background-color: ${props => props.theme.mainColors.primary};
+  color: ${props => props.theme.mainColors.textColor};
   border-radius: 3px;
   border: 1px solid silver;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.18);

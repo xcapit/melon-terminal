910ea8cbbe7062a77bad6a3d785d219af3b95d35
Ivan Herger <ivan.herger@gmail.com>
2020-02-25 08:00:55 +0100

    Improve right-alignment of gav, nav, etc on factsheet

--- src/components/Routes/Fund/FundOverview/FundFactSheet/FundFactSheet.tsx
+++ src/components/Routes/Fund/FundOverview/FundFactSheet/FundFactSheet.tsx
@@ -19,6 +19,7 @@ import { standardDeviation } from '~/utils/finance';
 import { TwitterLink } from '~/components/Common/TwitterLink/TwitterLink';
 import { useAccount } from '~/hooks/useAccount';
 import { TokenValue } from '~/components/Common/TokenValue/TokenValue';
+import { range } from 'ramda';
 
 export interface NormalizedCalculation {
   sharePrice: BigNumber;
@@ -30,6 +31,12 @@ export interface NormalizedCalculation {
 export interface FundFactSheetProps {
   address: string;
 }
+
+export const numberPadding = (digits: number, maxDigits: number) => {
+  return range(0, maxDigits - digits)
+    .map(() => String.fromCharCode(160))
+    .join('');
+};
 
 export const FundFactSheet: React.FC<FundFactSheetProps> = ({ address }) => {
   const [fund, fundQuery] = useFundDetailsQuery(address);
@@ -86,6 +93,12 @@ export const FundFactSheet: React.FC<FundFactSheetProps> = ({ address }) => {
       timestamp: new BigNumber(item.timestamp).toNumber(),
     };
   });
+
+  const gavDigits = accounting?.grossAssetValue.integerValue().toString().length;
+  const navDigits = accounting?.netAssetValue.integerValue().toString().length;
+  const sharesDigits = shares?.totalSupply.integerValue().toString().length;
+  const sharePriceDigits = accounting?.sharePrice.integerValue().toString().length;
+  const maxDigits = Math.max(gavDigits || 0, navDigits || 0, sharesDigits || 0, sharePriceDigits || 0);
 
   const numbersLength = normalizedCalculations.length;
   const firstChange = (normalizedCalculations?.[0] || []) as NormalizedCalculation;
@@ -159,25 +172,29 @@ export const FundFactSheet: React.FC<FundFactSheetProps> = ({ address }) => {
       </DictionaryEntry>
       <DictionaryEntry>
         <DictionaryLabel>Gross asset value (GAV)</DictionaryLabel>
-        <DictionaryData textAlign="right">
+        <DictionaryData>
+          <span>{numberPadding(gavDigits || 0, maxDigits)}</span>
           <TokenValue value={accounting?.grossAssetValue} symbol="WETH" decimals={0} />
         </DictionaryData>
       </DictionaryEntry>
       <DictionaryEntry>
         <DictionaryLabel>Net asset value (NAV)</DictionaryLabel>
-        <DictionaryData textAlign="right">
+        <DictionaryData>
+          <span>{numberPadding(navDigits || 0, maxDigits)}</span>
           <TokenValue value={accounting?.netAssetValue} symbol="WETH" decimals={0} />
         </DictionaryData>
       </DictionaryEntry>
       <DictionaryEntry>
         <DictionaryLabel>Total number of shares</DictionaryLabel>
-        <DictionaryData textAlign="right">
-          <TokenValue value={shares?.totalSupply} decimals={0} />
+        <DictionaryData>
+          <span>{numberPadding(sharesDigits || 0, maxDigits)}</span>
+          <TokenValue value={shares?.totalSupply} symbol="&nbsp;&nbsp;&nbsp;&nbsp;" decimals={0} />
         </DictionaryData>
       </DictionaryEntry>
       <DictionaryEntry>
         <DictionaryLabel>Share price</DictionaryLabel>
-        <DictionaryData textAlign="right">
+        <DictionaryData>
+          <span>{numberPadding(sharePriceDigits || 0, maxDigits)}</span>
           <TokenValue value={accounting?.sharePrice} symbol="WETH" decimals={0} />
         </DictionaryData>
       </DictionaryEntry>

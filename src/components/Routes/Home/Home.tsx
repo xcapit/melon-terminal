d3464cd5831ee7a7f90cc1d321ad2e242efe625c
Ivan Herger <ivan.herger@gmail.com>
2020-03-10 16:19:25 +0100

    Remove temporary error message

--- src/components/Routes/Home/Home.tsx
+++ src/components/Routes/Home/Home.tsx
@@ -3,7 +3,6 @@ import { Grid, GridCol, GridRow } from '~/storybook/components/Grid/Grid';
 import { Container } from '~/storybook/components/Container/Container';
 import { FundMetrics } from '~/components/Routes/Home/FundMetrics/FundMetrics';
 import { FundOverview } from '~/components/Routes/Home/FundOverview/FundOverview';
-import { NotificationBar, NotificationContent } from '~/storybook/components/NotificationBar/NotificationBar';
 
 export const Home: React.FC = () => {
   return (
@@ -11,14 +10,6 @@ export const Home: React.FC = () => {
       <Grid>
         <GridRow>
           <GridCol>
-            <NotificationBar kind="error">
-              <NotificationContent>
-                The Melon subgraph, which provides overview data for the Melon Terminal, is under maintenance and will
-                be operational again in a couple of hours.
-                <br />
-                Meanwhile all funds are operational i.e. you can invest and trade.
-              </NotificationContent>
-            </NotificationBar>
             <FundMetrics />
           </GridCol>
         </GridRow>

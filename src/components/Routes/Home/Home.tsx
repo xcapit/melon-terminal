3388b81d8491983ef71b8eadf2cba650e95bb3b7
Ivan Herger <ivan.herger@gmail.com>
2020-03-10 11:01:15 +0100

    Add temporary error message for subgraph failure

--- src/components/Routes/Home/Home.tsx
+++ src/components/Routes/Home/Home.tsx
@@ -3,6 +3,7 @@ import { Grid, GridCol, GridRow } from '~/storybook/components/Grid/Grid';
 import { Container } from '~/storybook/components/Container/Container';
 import { FundMetrics } from '~/components/Routes/Home/FundMetrics/FundMetrics';
 import { FundOverview } from '~/components/Routes/Home/FundOverview/FundOverview';
+import { NotificationBar, NotificationContent } from '~/storybook/components/NotificationBar/NotificationBar';
 
 export const Home: React.FC = () => {
   return (
@@ -10,6 +11,13 @@ export const Home: React.FC = () => {
       <Grid>
         <GridRow>
           <GridCol>
+            <NotificationBar kind="error">
+              <NotificationContent>
+                We are currently experiencing issues with the Melon subgraph, which provides overview data for the Melon
+                Terminal. We are working on a fix. <br />
+                All funds are operational, i.e. you can still invest and trade.
+              </NotificationContent>
+            </NotificationBar>
             <FundMetrics />
           </GridCol>
         </GridRow>

bbd358a970b4acaf7e11143d596fca2e9c08827a
ishak-lm <53437205+ishak-lm@users.noreply.github.com>
2020-04-21 09:16:25 +0200

    Update FundExchanges form to use Formik and new Form component (#506)

--- src/components/Routes/Fund/FundManagement/FundExchanges/FundExchanges.tsx
+++ src/components/Routes/Fund/FundManagement/FundExchanges/FundExchanges.tsx
@@ -1,25 +1,22 @@
 import React, { useEffect, useMemo, useRef } from 'react';
 import * as Yup from 'yup';
-import { useForm, FormContext } from 'react-hook-form';
-import { Trading } from '@melonproject/melonjs';
+import { Trading, DeployedEnvironment, ExchangeDefinition } from '@melonproject/melonjs';
 import { useEnvironment } from '~/hooks/useEnvironment';
-import { useTransaction } from '~/hooks/useTransaction';
-import { Button } from '~/storybook/Button/Button';
+import { useTransaction, TransactionHookValues } from '~/hooks/useTransaction';
+import { Button } from '~/components/Form/Button/Button';
 import { TransactionModal } from '~/components/Common/TransactionModal/TransactionModal';
 import { Block, BlockActions } from '~/storybook/Block/Block';
 import { SectionTitle } from '~/storybook/Title/Title';
 import { Spinner } from '~/storybook/Spinner/Spinner';
-import { Checkboxes } from '~/storybook/Checkbox/Checkbox';
 import { useAccount } from '~/hooks/useAccount';
 import { useFundExchangesQuery } from './FundExchanges.query';
 import { TransactionDescription } from '~/components/Common/TransactionModal/TransactionDescription';
+import { Form, useFormik } from '~/components/Form/Form';
+import { CheckboxGroup, CheckboxGroupOption } from '~/components/Form/CheckboxGroup/CheckboxGroup';
+import { AccountContextValue } from '~/components/Contexts/Account/Account';
 
 export interface ExchangesProps {
   address: string;
-}
-
-export interface ExchangesForm {
-  exchanges: string[];
 }
 
 export const FundExchanges: React.FC<ExchangesProps> = ({ address }) => {
@@ -39,24 +36,6 @@ export const FundExchanges: React.FC<ExchangesProps> = ({ address }) => {
     exchangesRef.current = exchanges;
   }, [exchanges]);
 
-  const form = useForm<ExchangesForm>({
-    mode: 'onSubmit',
-    reValidateMode: 'onChange',
-    validationSchema: Yup.object().shape({
-      exchanges: Yup.array<string>()
-        .compact()
-        .test('at-least-one', "You didn't select a new exchange.", (value: string[]) => {
-          const options = exchangesRef.current;
-          return value.some(selected => selected && !options.some(available => available.id === selected))!;
-        })
-        .test('only-one', 'You can only add one exchange at a time.', (value: string[]) => {
-          const options = exchangesRef.current;
-          const add = value.filter(selected => selected && !options.some(available => available.id === selected))!;
-          return add.length === 1;
-        }),
-    }),
-  });
-
   if (query.loading) {
     return (
       <Block>
@@ -75,37 +54,94 @@ export const FundExchanges: React.FC<ExchangesProps> = ({ address }) => {
       disabled: !!exchanges?.some(enabled => enabled.id === exchange.id),
     }));
 
-  const submit = form.handleSubmit(async data => {
-    const add = data.exchanges.find(selected => selected && !exchanges.some(available => available.id === selected))!;
-    const exchange = environment.getExchange(add);
-    const address = details?.fund?.routes?.trading?.address;
-    const trading = new Trading(environment, address!);
-    const tx = trading.addExchange(account.address!, exchange.exchange, exchange.adapter);
-    transaction.start(tx, 'Add exchange');
-  });
-
   return (
     <Block>
-      <FormContext {...form}>
-        <form onSubmit={submit}>
-          <SectionTitle>Define Allowed Exchanges</SectionTitle>
-          <p>As a fund manager, you can trade on any of the exchanges selected below.</p>
-
-          <Checkboxes options={options} name="exchanges" />
-
-          <BlockActions>
-            <Button type="button" onClick={submit}>
-              Set Allowed Exchanges
-            </Button>
-          </BlockActions>
-        </form>
-      </FormContext>
-
+      <FundExchangesForm
+        exchanges={exchanges}
+        exchangesRef={exchangesRef}
+        details={details}
+        options={options}
+        environment={environment}
+        account={account}
+        transaction={transaction}
+      />
       <TransactionModal transaction={transaction}>
         <TransactionDescription title="Add exchange">
           You are adding an exchange to the list of allowed exchanges.
         </TransactionDescription>
       </TransactionModal>
     </Block>
+  );
+};
+
+const validationSchema = Yup.object().shape({
+  exchanges: Yup.array<string>()
+    .compact()
+    .test('at-least-one', "You didn't select a new exchange.", function(value: string[]) {
+      const options = (this.options.context as any).exchangesRef.current as ExchangeDefinition[];
+      return value.some(selected => selected && !options.some((available: any) => available.id === selected))!;
+    })
+    .test('only-one', 'You can only add one exchange at a time.', function(value: string[]) {
+      const options = (this.options.context as any).exchangesRef.current as ExchangeDefinition[];
+      const add = value.filter(selected => selected && !options.some((available: any) => available.id === selected))!;
+      return add.length === 1;
+    }),
+});
+
+interface FundExchangesFormProps {
+  exchangesRef: any;
+  exchanges: ExchangeDefinition[];
+  details: any;
+  options: CheckboxGroupOption[];
+  account: AccountContextValue;
+  environment: DeployedEnvironment;
+  transaction: TransactionHookValues;
+}
+
+const FundExchangesForm: React.FC<FundExchangesFormProps> = ({
+  exchangesRef,
+  exchanges,
+  details,
+  options,
+  account,
+  environment,
+  transaction,
+}) => {
+  const initialValues = {
+    exchanges: [],
+  };
+
+  const validationContext = React.useMemo(
+    () => ({
+      account,
+      environment,
+      exchangesRef,
+    }),
+    [account, environment, exchangesRef]
+  );
+
+  const formik = useFormik({
+    validationSchema,
+    validationContext,
+    initialValues,
+    onSubmit: data => {
+      const add = data.exchanges.find(selected => selected && !exchanges.some(available => available.id === selected))!;
+      const exchange = environment.getExchange(add);
+      const address = details?.fund?.routes?.trading?.address;
+      const trading = new Trading(environment, address!);
+      const tx = trading.addExchange(account.address!, exchange.exchange, exchange.adapter);
+      transaction.start(tx, 'Add exchange');
+    },
+  });
+
+  return (
+    <Form formik={formik}>
+      <SectionTitle>Define Allowed Exchanges</SectionTitle>
+      <p>As a fund manager, you can trade on any of the exchanges selected below.</p>
+      <CheckboxGroup name="exchanges" options={options} />
+      <BlockActions>
+        <Button type="submit">Set Allowed Exchanges</Button>
+      </BlockActions>
+    </Form>
   );
 };

9fd58ed741f6c875046c5ace9a510d06c85486ab
Ivan Herger <ivan.herger@gmail.com>
2020-05-19 11:09:31 +0200

    Fix TokenSwap border color

--- src/components/Form/TokenSwap/TokenSwap.styles.ts
+++ src/components/Form/TokenSwap/TokenSwap.styles.ts
@@ -13,6 +13,8 @@ export interface TokenSwapButtonProps {
 export const TokenSwapButton = styled.div<TokenSwapButtonProps>`
   padding: ${(props) => props.theme.spaceUnits.xs} ${(props) => props.theme.spaceUnits.m};
   cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
-  border-right: ${(props) => props.theme.border.borderDefault};
+  border-color: ${(props) => props.theme.mainColors.secondaryDarkAlpha};
+  border-style: solid;
+  border-width: 0px 1px 0px 0px;
   background-color: ${(props) => (props.disabled ? props.theme.mainColors.secondary : props.theme.mainColors.primary)};
 `;
